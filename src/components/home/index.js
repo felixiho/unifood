39b67138039622cc597f3e3989a9e0aa07498193
Okeke Felix <okekefelix1@gmail.com>
2019-05-12 17:47:00 +0100

    Installed material-uI

---
+++ src/components/home/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function CenteredGrid(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container  >
+        <Grid item xs={12}> 
+            <h1>jams</h1>
+        </Grid> 
+      </Grid>
+    </div>
+  );
+}
+
+CenteredGrid.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CenteredGrid);
